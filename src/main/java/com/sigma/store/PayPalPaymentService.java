f81a2845c79aade69c697a39db5f4868d4c5f396
u-gul <ujjwal.dev17@gmail.com>
2026-04-26 17:55:09 +0530

    create PayPalPaymentService class

---
+++ src/main/java/com/sigma/store/PayPalPaymentService.java
@@ -0,0 +1,9 @@
+package com.sigma.store;
+
+public class PayPalPaymentService implements PaymentService{
+    @Override
+    public void processPayment(double amount) {
+        System.out.println("PayPal");
+        System.out.println("Amount: " + amount + " paid.");
+    }
+}
