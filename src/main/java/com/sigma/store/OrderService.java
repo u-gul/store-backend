f829f98936d9b9bc096cb9fbf02cd8c825e97400
u-gul <ujjwal.dev17@gmail.com>
2026-04-26 18:10:59 +0530

    removed the constructor for time being to test dependency injection via setter

--- src/main/java/com/sigma/store/OrderService.java
+++ src/main/java/com/sigma/store/OrderService.java
@@ -4,9 +4,9 @@ public class OrderService {
 
     private PaymentService paymentService;
 
-    public OrderService(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+//    public OrderService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
 
     public void placeOrder() {
         paymentService.processPayment(10);
