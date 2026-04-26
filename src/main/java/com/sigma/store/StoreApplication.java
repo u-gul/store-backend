9f1b2f64874b27148943a2fe49eafec17bb29688
u-gul <ujjwal.dev17@gmail.com>
2026-04-26 17:48:53 +0530

    tried to run it as normal java application instead of springboot app for time being

--- src/main/java/com/sigma/store/StoreApplication.java
+++ src/main/java/com/sigma/store/StoreApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StoreApplication.class, args);
+//        SpringApplication.run(StoreApplication.class, args);
+        OrderService orderService = new OrderService(new StripePaymentService());
+        orderService.placeOrder();
     }
 }
