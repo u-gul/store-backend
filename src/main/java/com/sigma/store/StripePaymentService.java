519a7bada04e12cfaf933ca74437a63f28a80dd6
u-gul <ujjwal.dev17@gmail.com>
2026-04-27 12:50:25 +0530

    add Primary annotation to StripePaymentService

--- src/main/java/com/sigma/store/StripePaymentService.java
+++ src/main/java/com/sigma/store/StripePaymentService.java
@@ -1,5 +1,10 @@
 package com.sigma.store;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
 public class StripePaymentService implements PaymentService {
 
     @Override
