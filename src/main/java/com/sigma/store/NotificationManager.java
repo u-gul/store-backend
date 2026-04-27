573f41b4e1261fcb2798d3e6597c3c38354d5cce
u-gul <ujjwal.dev17@gmail.com>
2026-04-27 13:09:23 +0530

    mark as Spring bean

--- src/main/java/com/sigma/store/NotificationManager.java
+++ src/main/java/com/sigma/store/NotificationManager.java
@@ -1,10 +1,14 @@
 package com.sigma.store;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NotificationManager {
 
     private NotificationService notificationService;
 
-    public NotificationManager(NotificationService notificationService) {
+    public NotificationManager(@Qualifier("sms") NotificationService notificationService) {
         this.notificationService = notificationService;
     }
 
--- src/main/java/com/sigma/store/SMSNotificationService.java
+++ src/main/java/com/sigma/store/SMSNotificationService.java
@@ -1,8 +1,11 @@
 package com.sigma.store;
 
+import org.springframework.stereotype.Service;
+
+@Service("sms")
 public class SMSNotificationService implements NotificationService{
     @Override
     public void send(String message) {
-        System.out.println("SMS Notification " + message);
+        System.out.println("SMS Notification ->" + message);
     }
 }
