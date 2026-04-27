e0a6485a2f72342014fa0fdec82d82517774872f
u-gul <ujjwal.dev17@gmail.com>
2026-04-27 13:02:50 +0530

    create NotificationManager class

---
+++ src/main/java/com/sigma/store/NotificationManager.java
@@ -0,0 +1,14 @@
+package com.sigma.store;
+
+public class NotificationManager {
+
+    private NotificationService notificationService;
+
+    public NotificationManager(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    public void sendNotification(String message) {
+        notificationService.send(message);
+    }
+}
