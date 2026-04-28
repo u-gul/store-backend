ae25aee4e41e546c584c561b6598c846e323c007
u-gul <ujjwal.dev17@gmail.com>
2026-04-28 12:28:07 +0000

    create tag entity

---
+++ src/main/java/com/sigma/store/entities/Tag.java
@@ -0,0 +1,21 @@
+package com.sigma.store.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "tags")
+@Getter
+@Setter
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+}
