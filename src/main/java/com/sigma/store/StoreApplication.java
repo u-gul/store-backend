6c82cb47f947f3568870e7f9190d8aa2722df050
u-gul <ujjwal.dev17@gmail.com>
2026-04-28 04:49:41 +0000

    add UserService calling logic in main method

--- src/main/java/com/sigma/store/StoreApplication.java
+++ src/main/java/com/sigma/store/StoreApplication.java
@@ -9,11 +9,17 @@ public class StoreApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-        var orderService = context.getBean(OrderService.class);
+//        var orderService = context.getBean(OrderService.class);
 //        OrderService orderService = new OrderService(new PayPalPaymentService());
 //        orderService.setPaymentService(new PayPalPaymentService());
-        orderService.placeOrder();
+//        orderService.placeOrder();
 //        var notificationManager = context.getBean(NotificationManager.class);
 //        notificationManager.sendNotification("Your bill is due, please pay asap");
+
+        UserService userService = context.getBean(UserService.class);
+        userService.registerUser(new User(101, "hitman@gmail.com", "abc", "Hitman"));
+        userService.registerUser(new User(102, "vk@gmail.com", "xyz", "VK"));
+        System.out.println("Printing all users below");
+        userService.getAllUsers();
     }
 }
