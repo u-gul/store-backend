fe6bffee74a90dbea4c0e9b9e57370c3747c030c
u-gul <ujjwal.dev17@gmail.com>
2026-04-28 09:59:38 +0000

    replaced getters & setters with lombok annotations

--- src/main/java/com/sigma/store/entities/User.java
+++ src/main/java/com/sigma/store/entities/User.java
@@ -1,9 +1,13 @@
 package com.sigma.store.entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
+@Getter
+@Setter
 public class User {
 
     @Id
@@ -20,35 +24,4 @@ public class User {
     @Column(nullable = false, name = "password")
     private String password;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
