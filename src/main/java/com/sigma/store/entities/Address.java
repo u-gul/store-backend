41a148ec987bbf3ac56b5cad173487daef012b4e
u-gul <ujjwal.dev17@gmail.com>
2026-04-28 12:28:00 +0000

    create address entity

---
+++ src/main/java/com/sigma/store/entities/Address.java
@@ -0,0 +1,28 @@
+package com.sigma.store.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "addresses")
+@Getter
+@Setter
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "street")
+    private String street;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "zip")
+    private String zip;
+
+    @Column(name = "state")
+    private String state;
+}
