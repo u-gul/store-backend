2ab822e81a4b63287b8528bc055861ef59796539
u-gul <ujjwal.dev17@gmail.com>
2026-04-28 10:04:22 +0000

    add constructors

--- src/main/java/com/sigma/store/entities/User.java
+++ src/main/java/com/sigma/store/entities/User.java
@@ -1,13 +1,17 @@
 package com.sigma.store.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     @Id
