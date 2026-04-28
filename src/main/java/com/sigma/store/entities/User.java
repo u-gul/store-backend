f87cb7f56b1bbbf8c9595e477c8683b39577192b
u-gul <ujjwal.dev17@gmail.com>
2026-04-28 10:17:33 +0000

    add builder

--- src/main/java/com/sigma/store/entities/User.java
+++ src/main/java/com/sigma/store/entities/User.java
@@ -1,10 +1,7 @@
 package com.sigma.store.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "users")
@@ -12,6 +9,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class User {
 
     @Id
@@ -27,5 +25,4 @@ public class User {
 
     @Column(nullable = false, name = "password")
     private String password;
-
 }
