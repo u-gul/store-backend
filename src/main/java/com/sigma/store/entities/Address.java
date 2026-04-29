8df56c9d86eb96f38decb52375c95c06275750cf
u-gul <ujjwal.dev17@gmail.com>
2026-04-29 01:26:55 +0000

    implement oneToMany relationship

--- src/main/java/com/sigma/store/entities/Address.java
+++ src/main/java/com/sigma/store/entities/Address.java
@@ -7,6 +7,10 @@ import lombok.*;
 @Table(name = "addresses")
 @Getter
 @Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Address {
 
     @Id
@@ -25,4 +29,9 @@ public class Address {
 
     @Column(name = "state")
     private String state;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @ToString.Exclude
+    private User user;
 }
--- src/main/java/com/sigma/store/entities/User.java
+++ src/main/java/com/sigma/store/entities/User.java
@@ -3,12 +3,16 @@ package com.sigma.store.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "users")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 @Builder
 public class User {
 
@@ -25,4 +29,18 @@ public class User {
 
     @Column(name = "password")
     private String password;
+
+    @OneToMany(mappedBy = "user")
+    @Builder.Default
+    private List<Address> addresses = new ArrayList<>();
+
+    public void addAddress(Address address) {
+        addresses.add(address);
+        address.setUser(this);
+    }
+
+    public void removeAddress(Address address) {
+        addresses.remove(address);
+        address.setUser(null);
+    }
 }
