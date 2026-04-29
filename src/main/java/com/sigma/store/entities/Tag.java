566522ff465ab2dce637a05051c14c22fc246b3d
u-gul <ujjwal.dev17@gmail.com>
2026-04-29 02:29:57 +0000

    implement manyToMany relationship

--- src/main/java/com/sigma/store/entities/Tag.java
+++ src/main/java/com/sigma/store/entities/Tag.java
@@ -2,12 +2,19 @@ package com.sigma.store.entities;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "tags")
 @Getter
 @Setter
+@NoArgsConstructor
+@ToString
 public class Tag {
 
     @Id
@@ -18,4 +25,11 @@ public class Tag {
     @Column(name = "name")
     private String name;
 
+    @ToString.Exclude
+    @ManyToMany(mappedBy = "tags")
+    private Set<User> users = new HashSet<>();
+
+    public Tag(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/sigma/store/entities/User.java
+++ src/main/java/com/sigma/store/entities/User.java
@@ -4,7 +4,9 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Table(name = "users")
@@ -42,5 +44,20 @@ public class User {
     public void removeAddress(Address address) {
         addresses.remove(address);
         address.setUser(null);
+    }
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_tags",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    @Builder.Default
+    private Set<Tag> tags = new HashSet<>();
+
+    public void addTag(String tagName) {
+        Tag tag = new Tag(tagName);
+        tags.add(tag);
+        tag.getUsers().add(this);
     }
 }
