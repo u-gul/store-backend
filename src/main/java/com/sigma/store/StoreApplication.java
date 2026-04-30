831af46447358e667af5054ea5d777f28be0b9ca
u-gul <ujjwal.dev17@gmail.com>
2026-04-30 13:55:13 +0530

    remove unused import

--- src/main/java/com/sigma/store/StoreApplication.java
+++ src/main/java/com/sigma/store/StoreApplication.java
@@ -1,7 +1,6 @@
 package com.sigma.store;
 
 import com.sigma.store.entities.User;
-import com.sigma.store.unnecessary.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
