09aec2d03f8774e3f467f6c7f121feed4222ae18
u-gul <ujjwal.dev17@gmail.com>
2026-05-05 17:49:25 +0530

    changes

--- src/main/java/com/sigma/store/repositories/ProductRepository.java
+++ src/main/java/com/sigma/store/repositories/ProductRepository.java
@@ -1,6 +1,11 @@
 package com.sigma.store.repositories;
 
+import com.sigma.store.dtos.ProductSummary;
+import com.sigma.store.dtos.ProductSummaryDTO;
+import com.sigma.store.dtos.ProductSummaryRecord;
+import com.sigma.store.entities.Category;
 import com.sigma.store.entities.Product;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -44,6 +49,7 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
 
     //Null
     List<Product> findByDescriptionNull();
+
     List<Product> findByDescriptionNotNull();
 
     //Multiple Conditions
@@ -55,17 +61,29 @@ public interface ProductRepository extends CrudRepository<Product, Long> {
     //LIMIT/TOP
 
     List<Product> findTop5ByNameOrderByPriceAsc(String name);
+
     List<Product> findFirst5ByNameLikeOrderByPriceDesc(String name);
 
     //Find products whose prices are in given range and sort by name
 //    List<Product> findByPriceBetweenOrderByName(BigDecimal min, BigDecimal max);
     //SQL or JPQL
     @Query(value = "Select * from products p where p.price between :min and :max order by p.name", nativeQuery = true)
-    List<Product> findProducts(@Param("min") BigDecimal min,@Param("max") BigDecimal max);
+    List<Product> findProducts(@Param("min") BigDecimal min, @Param("max") BigDecimal max);
 
     @Query("Select p from Product p where p.price between :min and :max order by p.name")
     List<Product> findProducts2(@Param("min") BigDecimal min, @Param("max") BigDecimal max);
 
     @Query("select p from Product p join p.category where p.price between :min and :max order by p.name")
     List<Product> findProducts3(@Param("min") BigDecimal min, @Param("max") BigDecimal max);
+
+    @Query("select count(p) from Product p where p.price between :min and :max")
+    long countProducts(@Param("min") BigDecimal min, @Param("max") BigDecimal max);
+
+    @Modifying
+    @Query("update Product p set p.price = :newPrice where p.category.id = :categoryId")
+    void updatePriceByCategory(BigDecimal newPrice, Byte categoryId);
+
+
+    @Query("select p from Product p where p.category = :category")
+    List<ProductSummaryDTO> findProductsCategory(@Param("category") Category category);
 }
