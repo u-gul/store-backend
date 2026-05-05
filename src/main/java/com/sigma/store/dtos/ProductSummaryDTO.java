94056cf02650e298578ee63e6f2f9065dc6c06ce
u-gul <ujjwal.dev17@gmail.com>
2026-05-05 17:49:35 +0530

    changes

---
+++ src/main/java/com/sigma/store/dtos/ProductSummaryDTO.java
@@ -0,0 +1,11 @@
+package com.sigma.store.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
+public class ProductSummaryDTO {
+    private Long id;
+    private String name;
+}
