4afc73adbebc92fb2ef35cb0d9e938848bf8aa5c
u-gul <ujjwal.dev17@gmail.com>
2026-05-05 02:53:07 +0000

    changes to StoreApplication

--- src/main/java/com/sigma/store/StoreApplication.java
+++ src/main/java/com/sigma/store/StoreApplication.java
@@ -1,16 +1,18 @@
 package com.sigma.store;
 
-import com.sigma.store.services.UserService;
+import com.sigma.store.services.ProductService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+
+import java.math.BigDecimal;
 
 @SpringBootApplication
 public class StoreApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
-        UserService service = context.getBean(UserService.class);
-        service.manageProducts();
+        var productService = context.getBean(ProductService.class);
+        productService.getProductBetweenPrice(BigDecimal.valueOf(2.00), BigDecimal.valueOf(9.99));
     }
 }
