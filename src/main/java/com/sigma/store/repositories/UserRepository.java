675ca7e83ead83d4d333b3a23d8436cf9477ded2
u-gul <ujjwal.dev17@gmail.com>
2026-05-05 17:49:43 +0530

    changes

--- src/main/java/com/sigma/store/repositories/UserRepository.java
+++ src/main/java/com/sigma/store/repositories/UserRepository.java
@@ -1,8 +1,13 @@
 package com.sigma.store.repositories;
 
 import com.sigma.store.entities.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    @EntityGraph(attributePaths = "tags")
+    Optional<User> findByEmail(String email);
 }
